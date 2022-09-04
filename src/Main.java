e8a1f0351fa6a2d30e1f3bc415d9b2203c54fa81
Ilyailyou <yaaponia@gmail.com>
2022-09-04 21:36:01 +0500

    First lambda commit

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,16 @@ public class Main {
         list.add(new Person("Иисус", "Христос", 33));
         list.add(new Person("Стас", "Барецкий", 50));
         list.add(new Person("Абрахам", "Ван Хель Синг", 3));
-        Collections.sort(list, new PersonComparator(1));
+        Comparator<Person> comparator = (Person o1, Person o2) -> {
+            int count1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+            int count2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+            if(count1 != count2 && !(count1>=3 && count2>=3)) {
+                return Integer.compare(count1, count2);
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
+        Collections.sort(list, comparator);
         System.out.println(list);
-        Collections.sort(list, new PersonComparator(3));
-        System.out.println(list);
-
     }
 }
